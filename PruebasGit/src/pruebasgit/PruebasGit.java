07d0726e371f2def59b6c7891d9841c27e31fc4d
AlbertoParedesOtero <111366679+AlbertoParedesOtero@users.noreply.github.com>
2022-08-17 15:17:25 +0200

    Update PruebasGit.java

--- PruebasGit/src/pruebasgit/PruebasGit.java
+++ PruebasGit/src/pruebasgit/PruebasGit.java
@@ -17,6 +17,7 @@ public class PruebasGit {
     public static void main(String[] args) {
         System.out.println("proyecto --- v2");
         System.out.println("Esto lo he hecho desde el repositorio");
+        System.out.println("proyecto --- v2");
         int i=0;
         while(i<3){
             System.out.println("i: "+i);
